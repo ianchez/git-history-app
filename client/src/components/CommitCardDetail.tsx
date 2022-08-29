35d317703f7798f394088c0e442c69faf3e8be87
Seba Sanchez <lanchez@live.cl>
2022-08-29 15:19:58 -0400

    refactor: minor style details on CommitCardDetail

--- client/src/components/CommitCardDetail.tsx
+++ client/src/components/CommitCardDetail.tsx
@@ -8,12 +8,12 @@ interface CommitCardProps {
 const CommitCardDetail = ({ commit }: CommitCardProps) => {
   return (
     <Box sx={{ width: '96%' }}>
-      <Card variant="outlined" sx={{ marginBottom: 2, backgroundColor: 'rgb(31 41 55)' }}>
+      <Card variant="outlined" sx={{ marginBottom: 2, backgroundColor: 'rgb(21 31 35)' }}>
         <CardContent>
           <Typography sx={{ fontSize: 12 }} color="text.secondary" gutterBottom>
             {(new Date(commit.commit.author.date)).toLocaleString()}
           </Typography>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          <Typography sx={{ fontSize: 14 }} gutterBottom>
             {`Author: ${commit.commit.author.name}`}
           </Typography>
           <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
@@ -22,8 +22,8 @@ const CommitCardDetail = ({ commit }: CommitCardProps) => {
           <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
             {`SHA: ${commit.sha}`}
           </Typography>
-          <Typography sx={{ fontSize: 14 }} color="text.secondary">
-            {'URL: '}<Link underline="hover">{commit.htmlURL}</Link>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" >
+            {'URL: '}<Link underline="hover" href={commit.htmlURL}>{commit.htmlURL}</Link>
           </Typography>
         </CardContent>
       </Card>
