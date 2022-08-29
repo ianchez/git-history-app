65660f6b56961699f21a9e12389f8728a7f7d2dd
Seba Sanchez <lanchez@live.cl>
2022-08-29 14:16:58 -0400

    chore: include Github PAT token configuration to avoid limited request to the Github API

--- server/src/commits/commits.service.ts
+++ server/src/commits/commits.service.ts
@@ -1,15 +1,25 @@
-import fetch from 'node-fetch';
+import fetch, { Headers } from 'node-fetch';
 
 import { Injectable } from '@nestjs/common';
 import { GetCommitsQuery } from './dto/get-commits-query.dto';
 import { normalizeCommitsResponse } from '../utils/commits.utils';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class CommitsService {
+  constructor(private readonly configService: ConfigService) {}
+
   async getAllCommits(queryParams: GetCommitsQuery): Promise<any> {
     try {
       const { username, repo } = queryParams;
-      return fetch(`https://api.github.com/repos/${username}/${repo}/commits`)
+      const headers = new Headers({
+        Authorization: this.configService.get<string>('GIT_AUTH_TOKEN'),
+      });
+
+      return fetch(`https://api.github.com/repos/${username}/${repo}/commits`, {
+        method: 'GET',
+        headers,
+      })
         .then((res) => res.json())
         .then((json) => normalizeCommitsResponse(json));
     } catch (error) {
