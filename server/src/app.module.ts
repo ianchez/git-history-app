5ba421ec62679fc44f94b84847213782a20f34e0
Seba Sanchez <lanchez@live.cl>
2022-08-30 09:45:07 -0400

    chore: getting ready for deploying backend to cloud services like Heroku

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,11 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { CommitsModule } from './commits/commits.module';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
     DatabaseModule,
     CommitsModule,
   ],
